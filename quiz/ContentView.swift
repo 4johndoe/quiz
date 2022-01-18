c2d366e04d442f91094984fbc2bc65fe4aec9d88
bogdan demchenko <bogdandemchenko@gmail.com>
2022-01-18 17:47:13 +0200

    Initial Commit

---
+++ quiz/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  quiz
+//
+//  Created by Bogdan Demchenko on 18.01.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ quiz/quizApp.swift
@@ -0,0 +1,17 @@
+//
+//  quizApp.swift
+//  quiz
+//
+//  Created by Bogdan Demchenko on 18.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct quizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
